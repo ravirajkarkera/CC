e96c9053a9fb7a512c2682ae73597f92dbc73802
Raviraj Karkera <ravirajkarkera80@gmail.com>
2024-01-18 09:57:12 +0530

    Change commit

--- sum.java
+++ sum.java
@@ -1,7 +1,8 @@
 public class sum{
-    public static void main(String[] args)
-        int a=5;
+    public static void main(String[] args){
+        int a=7;
         int b=6;
 
         System.out.println(a+b);
+    }
 }
